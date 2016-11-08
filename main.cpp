31fe6332feb65b5286f96c792a7f94c28affab43
Maxwell Sanders <maxwell.sanders@mavs.uta.edu>
2016-11-08 12:38:27 -0600

    Added a point of sale window that will hold customer and order commands

--- main.cpp
+++ main.cpp
@@ -3,11 +3,14 @@
 
 #include "login.h"
 #include "workshop.h"
+#include "pos.h"
 
 int main(void){
     login login_win;
     workshop workshop_win;
+    pos POS_win;
     login_win.show();
     workshop_win.show();
+    POS_win.show();
     Fl::run();
 }
---
+++ pos.h
@@ -0,0 +1,37 @@
+//maxwell Sanders 1001069652
+//This is the class for the salesman side of the salesfloor window, which will open the sales man windows
+
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Return_Button.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Output.H>
+#include <FL/Fl_Box.H>
+#include <Fl/Fl_Menu_Item.H>
+#include <Fl/Fl_Choice.H>
+#include <Fl/Fl_Menu.H>
+#include <Fl/Fl_Menu_Bar.H>
+
+#ifndef __pos_H
+#define __pos_H 2016
+class pos : public Fl_Window{
+    Fl_Button logout;   
+    Fl_Menu_Bar menu;
+    Fl_Menu_Item commands[14] = {
+	{"&Customers",FL_ALT+'c', 0, 0, FL_SUBMENU},
+	{"Create C&ustomer",FL_ALT+'u', 0, 0},
+	{"&View Customers",FL_ALT+'v', 0, 0},
+	{0},
+	{"&Orders",FL_ALT+'o', 0, 0, FL_SUBMENU},	
+	{"&Create Order",FL_ALT+'c', 0, 0},
+	{"&View Orders",FL_ALT+'v', 0, 0},
+    };
+   public:
+	pos() :
+	Fl_Window(1000,700,"Rob Robman's Rockin RoboShop - point of sale"),
+	logout(910,660,80,30, "Logout"),
+	menu(0,0,1000,20){
+	    menu.menu(commands);
+        };
+};
+#endif
