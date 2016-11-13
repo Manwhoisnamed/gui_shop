168ef41e7cb86152efcde253ab05e2e781b5ed80
Maxwell Sanders <maxwell.sanders@mavs.uta.edu>
2016-11-12 19:21:24 -0600

    added the final part adding window

--- Torso.h
+++ Torso.h
@@ -11,7 +11,10 @@
     int batterySpace;
   public:
     Torso(){};
-    Torso(string iname, int iSN, double iweight, double icost, string idescription, double idraw, int bspace) : RoboPart(iname, iSN, iweight, icost, idescription) , draw(idraw), batterySpace(bspace){}
+    Torso(string iname, int iSN, double iweight, double icost, string idescription, double idraw, int bspace): 
+    RoboPart(iname, iSN, iweight, icost, idescription), 
+    draw(idraw), 
+    batterySpace(bspace){}
     double getDraw();
     int getBSpace();
     int getASockets();
